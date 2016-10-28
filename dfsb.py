210c755b7315440bd84ea473929a468fcc2b8d90
Zhichuang Sun <zhisun@cs.stonybrook.edu>
2016-10-28 00:56:26 -0400

    add code for basic dfsb, and incomplete support for mrv
    Signed-off-by: Zhichuang Sun <zhisun@cs.stonybrook.edu>

--- dfsb.py
+++ dfsb.py
@@ -28,6 +28,10 @@ class CSP:
 			print("input file format does not match!")
 			exit(1)
 
+		self.n = N
+		self.m = M
+		self.k = K
+
 		self._adj_array_ = [[] for i in range(N)]
 		self._asgn_array_ = [-1] * N
 		i = 0
@@ -43,11 +47,95 @@ class CSP:
 		print(self._adj_array_)
 		print(self._asgn_array_)
 
-	def dfsb(self):
+	# basic version, no optimization
+	def dfsb(self, idx):
+		if idx == self.n:
+			self.print_res(self._asgn_array_)
+			return True
+
+		tag = False
+		for color in range(self.k):
+			if self.check(idx, color):
+				tag = True
+				self._asgn_array_[idx] = color
+				if self.dfsb(idx+1):
+					break
+			else:
+				continue
+
+		if tag == False:
+			return False
+		else:
+			return True
+
+	# compare color at @idx with every assigned variable to check conflicts
+	def check(self, idx, color):
+		for adj in self._adj_array_[idx]:
+			if adj >= idx:
+				continue
+			else:
+				if color == self._asgn_array_[adj]:
+					return False
+		return True
+
+	# prune using odering variables and values
+	def dfsb_order_var_wrapper(self):
+		#possible_value_array
+		pva = []
+		for x in range(self.n):
+			a = []
+			for c in range(self.k):
+				a.append(c)
+			pva.append(a)
+
+		# find variable with MRV(minimum remaining values)
+		asgn_array = [-1] * self.n		
+		self.dfsb_order_var(pva, asgn_array)
+
+	def dfsb_order_var(self, pva, asgn_array)
+		var = self.find_var(pva)
+		color = self.find_color(pva, var)
+		new_pva = self.construct_new_pva(pva, var, color)
+		asgn_array[var] = color
 		pass
 
-	def print_res(self):
+
+		
+	def find_color(pva, var):
+		# least pruned number
+		lpn = self.k + 1
+		c = -1
+		for color in pva[var]:
+			pn = 0
+			for neighbor in self._adj_array_[var]:
+				if color in pva[neighbor]:
+					pn += 1
+			if pn < lpn:
+				lpn = pn
+				c = color
+		return c
+		
+	# find variable with MRV(minimum remaining values)
+	def find_var(self, pva):
+		idx = -1
+		min_count = self.k + 1
+		for i in range(self.n):
+			if len(pva[i]) == 0:
+				continue
+			if len(pva[i]) < min_count:
+				min_count = len(pva[i])
+				idx = i
+		return idx
+
+
+	# prune using arc consistency
+	def dfsb_ac(self):
 		pass
+	def print_res(self, arr):
+		print("Success, a possible assignment:")
+		for x in arr:
+			print(x, end=" ")
+		print("")
 
 if __name__ == '__main__':
 
@@ -69,5 +157,4 @@ if __name__ == '__main__':
 		exit(1)
 
 	csp = CSP(fd_in, fd_out)
-	csp.dfsb()
-	csp.print_res()
+	csp.dfsb(0)
